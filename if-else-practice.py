4360e1cb24c89978727c648d736de8fa4526a5a8
Erin Grady <erinmariegrady@gmail.com>
2020-12-17 17:07:42 +0000

    initial commit

---
+++ if-else-practice.py
@@ -0,0 +1,17 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(raw_input().strip())
+
+# Given an integer, , perform the following conditional actions:
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of 2 to 5, print Not Weird
+# If  is even and in the inclusive range of 6 to 20, print Weird
+# If  is even and greater than 20 , print Not Weird
